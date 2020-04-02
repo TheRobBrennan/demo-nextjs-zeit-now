ae9866d6caecada9c8fb304e2e492999f4093a8e
Rob Brennan <rob@therobbrennan.com>
2020-04-01 19:56:40 -0700

    Converted Test page to TypeScript

--- pages/test.tsx
+++ pages/test.tsx
@@ -1,11 +1,10 @@
-import Layout from '../src/layout/Layout'
 import Link from 'next/link'
 
 export default () => {
   return (
-    <Layout>
+    <>
       <h1>Test</h1>
       <Link href='/'>Home</Link>
-    </Layout>
+    </>
   )
 }
